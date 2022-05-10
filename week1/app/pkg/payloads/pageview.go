88b6802e6f77fa52eb952a761a6b26090eb6f5b4
YeongSeok Kim <youngstone89@icloud.com>
2022-05-10 14:29:49 +0900

    added pageview, users payloads

--- week1/app/pkg/payloads/pageview.go
+++ week1/app/pkg/payloads/pageview.go
@@ -1,1 +1,37 @@
 package payloads
+
+import (
+	"event-data-pipeline/pkg/pipelines"
+	"sync"
+)
+
+var (
+	_ pipelines.Payload = (*pageviewPayload)(nil)
+
+	pageviewPayloadPool = sync.Pool{
+		New: func() interface{} { return new(pageviewPayload) },
+	}
+)
+
+type pageviewPayload struct {
+	viewtime int
+	userid   string
+	pageid   string
+}
+
+// Clone implements pipeline.Payload.
+func (p *pageviewPayload) Clone() pipelines.Payload {
+	newP := pageviewPayloadPool.Get().(*pageviewPayload)
+	newP.viewtime = p.viewtime
+	newP.userid = p.userid
+	newP.pageid = p.pageid
+	return newP
+}
+
+// MarkAsProcessed implements pipeline.Payload
+func (p *pageviewPayload) MarkAsProcessed() {
+	p.viewtime = 0
+	p.userid = ""
+	p.pageid = ""
+	pageviewPayloadPool.Put(p)
+}
--- week1/app/pkg/payloads/users.go
+++ week1/app/pkg/payloads/users.go
@@ -1,1 +1,38 @@
 package payloads
+
+import (
+	"event-data-pipeline/pkg/pipelines"
+	"sync"
+	"time"
+)
+
+var (
+	_ pipelines.Payload = (*usersPayload)(nil)
+
+	usersPayloadPool = sync.Pool{
+		New: func() interface{} { return new(usersPayload) },
+	}
+)
+
+type usersPayload struct {
+	userid       string
+	regionid     string
+	gender       string
+	registertime time.Time
+}
+
+// Clone implements pipeline.Payload.
+func (p *usersPayload) Clone() pipelines.Payload {
+	newP := usersPayloadPool.Get().(*usersPayload)
+
+	return newP
+}
+
+// MarkAsProcessed implements pipeline.Payload
+func (p *usersPayload) MarkAsProcessed() {
+	p.userid = ""
+	p.gender = ""
+	p.regionid = ""
+	p.registertime = time.Time{}
+	usersPayloadPool.Put(p)
+}

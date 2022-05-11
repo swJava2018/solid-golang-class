42ccdf7c70f5a6992d625a0602b1532b70305dbc
YeongSeok Kim <youngstone89@icloud.com>
2022-05-11 13:51:26 +0900

    added storage interface and facgtory

--- week1/app/pkg/storages/storage.go
+++ week1/app/pkg/storages/storage.go
@@ -1,1 +1,50 @@
 package storages
+
+import (
+	"event-data-pipeline/pkg/logger"
+	"fmt"
+	"strings"
+)
+
+type (
+	jsonObj = map[string]interface{}
+	jsonArr = []interface{}
+)
+type Storage interface {
+	Write(key string, path string, data []byte) (int, error)
+}
+
+type StorageFactory func(config jsonObj) Storage
+
+var storageFactories = make(map[string]StorageFactory)
+
+// Each storage implementation must Register itself
+func Register(name string, factory StorageFactory) {
+	logger.Debugf("Registering storage factory for %s", name)
+	if factory == nil {
+		logger.Panicf("Storage factory %s does not exist.", name)
+	}
+	_, registered := storageFactories[name]
+	if registered {
+		logger.Errorf("Storage factory %s already registered. Ignoring.", name)
+	}
+	storageFactories[name] = factory
+}
+
+// CreateStorage is a factory method that will create the named storage
+func CreateStorage(name string, config jsonObj) (Storage, error) {
+
+	factory, ok := storageFactories[name]
+	if !ok {
+		// Factory has not been registered.
+		// Make a list of all available datastore factories for logging.
+		availableStorages := make([]string, 0)
+		for k := range storageFactories {
+			availableStorages = append(availableStorages, k)
+		}
+		return nil, fmt.Errorf("invalid Storage name. Must be one of: %s", strings.Join(availableStorages, ", "))
+	}
+
+	// Run the factory with the configuration.
+	return factory(config), nil
+}

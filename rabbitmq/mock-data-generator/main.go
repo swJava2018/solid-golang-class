a119d040fc44676d17738a2a4ccfe146722bf2df
YeongSeok Kim <youngstone89@icloud.com>
2022-05-27 23:57:11 +0900

    added mock data generator

--- rabbitmq/mock-data-generator/main.go
+++ rabbitmq/mock-data-generator/main.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
-	host := flag.String("host", "amqp://guest:guest@:::5672/", "rabbitmq host you are connecting to")
+	host := flag.String("host", "amqp://user:bitnami@:::5672/", "rabbitmq host you are connecting to")
 	queue := flag.String("queue", "mock-data", "queue name you are publishing data to")
 	filePath := flag.String("file path of mock json data", "mock_data.json", "data file you are loading to publish")
 
 	flag.Parse()
-	client := rabbitmq.NewRabbitMQClient(*host, *queue, *queue)
-	producer := rabbitmq.NewRabbitMQProducer(client)
-	producer.Publish(*filePath)
+	client := rabbitmq.NewRabbitMQClient(*host)
+	p := rabbitmq.NewRabbitMQProducer(client)
+	p.QueueDeclare(*queue)
+	p.Publish(*filePath)
+	defer p.Close()
 }

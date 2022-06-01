79371d388a3c7d2eac60a18f7a4e7360e2ee3a4c
YeongSeok Kim <youngstone89@icloud.com>
2022-06-01 14:34:17 +0900

    noop processor table test

--- app/pkg/processors/noop_test.go
+++ app/pkg/processors/noop_test.go
@@ -11,58 +11,80 @@ import (
 )
 
 func TestNoopProcessor_Process(t *testing.T) {
-
+	testCases := []struct {
+		desc          string
+		processorName string
+		payload       payloads.Payload
+		testcase      string
+	}{
+		{
+			desc:          "valid payload",
+			processorName: "noop",
+			payload:       &ConcretePayload{id: 1},
+			testcase:      "valid",
+		},
+		{
+			desc:          "invalid payload",
+			processorName: "noop",
+			payload:       nil,
+			testcase:      "invalid",
+		},
+	}
 	os.Args = nil
 	os.Setenv("EDP_ENABLE_DEBUG_LOGGING", "true")
 	logger.Setup()
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			// 프로세서 생성
+			np, err := processors.CreateProcessor(tC.processorName, nil)
+			if err != nil {
+				t.Error(err)
+			}
+			// 스테이지 러너 생성
+			fifo := pipelines.FIFO(np)
 
-	// 프로세서 생성
-	np, err := processors.CreateProcessor("noop", nil)
-	if err != nil {
-		t.Error(err)
-	}
-	// 스테이지 러너 생성
-	fifo := pipelines.FIFO(np)
+			// 파라미터 생성
+			ctx, cancelFunc := context.WithCancel(context.Background())
 
-	// 파라미터 생성
-	ctx, cancelFunc := context.WithCancel(context.Background())
+			// Stage 개수는 1개로 고정
+			// 채널 개수는 Stage 개수 +1
+			stageCh := make([]chan payloads.Payload, 1+1)
+			// 에러채널 개수는 Stage 개수 +2
+			errCh := make(chan error, 1+2)
+			for i := 0; i < len(stageCh); i++ {
+				stageCh[i] = make(chan payloads.Payload)
+			}
 
-	// Stage 개수는 1개로 고정
-	// 채널 개수는 Stage 개수 +1
-	stageCh := make([]chan payloads.Payload, 1+1)
-	// 에러채널 개수는 Stage 개수 +2
-	errCh := make(chan error, 1+2)
-	for i := 0; i < len(stageCh); i++ {
-		stageCh[i] = make(chan payloads.Payload)
-	}
+			// FiFO Stage Runner 에게 넘길 파라미터 생성
+			wp := &workerParams{
+				stage: 0,
+				inCh:  stageCh[0],
+				outCh: stageCh[1],
+				errCh: errCh,
+			}
+			// StageRunner 구현체 FIFO 실행
+			// Goroutine 으로 실행
+			go fifo.Run(ctx, wp)
 
-	// FiFO Stage Runner 에게 넘길 파라미터 생성
-	wp := &workerParams{
-		stage: 0,
-		inCh:  stageCh[0],
-		outCh: stageCh[1],
-		errCh: errCh,
-	}
+			stageCh[0] <- tC.payload
 
-	// StageRunner 구현체 FIFO 실행
-	// Goroutine 으로 실행
-	go fifo.Run(ctx, wp)
-
-	// 채널에 페이로드 전달
-	// stageCh[0] <- &ConcretePayload{id: 1}
-	stageCh[0] <- nil
-
-	for {
-		select {
-		case err := <-errCh:
-			t.Errorf(err.Error())
-			cancelFunc()
-			return
-		case data := <-stageCh[1]:
-			t.Log(data)
-			cancelFunc()
-			return
-		}
+			for {
+				select {
+				case err := <-errCh:
+					if tC.testcase == "invalid" {
+						t.Log(err.Error())
+					} else {
+						t.Errorf(err.Error())
+					}
+					cancelFunc()
+					return
+				case data := <-stageCh[1]:
+					t.Log(data)
+					cancelFunc()
+					return
+				}
+			}
+		})
 	}
 }
 

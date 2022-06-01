b3c332296d9f92f18f1c8a0f0ee950f7be0f6593
YeongSeok Kim <youngstone89@icloud.com>
2022-06-01 15:41:42 +0900

    added validator into kafka default processor

--- app/pkg/processors/kafka_default_processor.go
+++ app/pkg/processors/kafka_default_processor.go
@@ -8,23 +8,29 @@ import (
 var _ Processor = new(KafkaDefaultProcessor)
 
 type KafkaDefaultProcessor struct {
+	Validator
 	KafkaMetaInjector
 }
 
 func init() {
-	Register("kafka_default_processor", NewKafkaDefaultProcessor)
+	Register("kafka_default", NewKafkaDefaultProcessor)
 }
 
 func NewKafkaDefaultProcessor(config jsonObj) Processor {
 	return &KafkaDefaultProcessor{
+		Validator{},
 		KafkaMetaInjector{},
 	}
 }
 
 func (k *KafkaDefaultProcessor) Process(ctx context.Context, p payloads.Payload) (payloads.Payload, error) {
-
+	//Validator struct embedding
+	err := k.Validate(ctx, p)
+	if err != nil {
+		return nil, err
+	}
 	//KafkaMetaInject struct embedding
-	p, err := k.KafkaMetaInjector.Process(ctx, p)
+	p, err = k.KafkaMetaInjector.Process(ctx, p)
 	if err != nil {
 		return nil, err
 	}
---
+++ app/pkg/processors/kakfa_default_processor_test.go
@@ -0,0 +1,91 @@
+package processors_test
+
+import (
+	"context"
+	"event-data-pipeline/pkg/logger"
+	"event-data-pipeline/pkg/payloads"
+	"event-data-pipeline/pkg/pipelines"
+	"event-data-pipeline/pkg/processors"
+	"os"
+	"testing"
+)
+
+type jsonObj = map[string]interface{}
+
+func TestKafkaDefaultProcessor_Process(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		processorName string
+		payload       payloads.Payload
+		testcase      string
+	}{
+		{
+			desc:          "valid payload",
+			processorName: "kafka_default",
+			payload:       &payloads.KafkaPayload{Value: make(jsonObj)},
+			testcase:      "valid",
+		},
+		{
+			desc:          "invalid payload",
+			processorName: "kafka_default",
+			payload:       nil,
+			testcase:      "invalid",
+		},
+	}
+	os.Args = nil
+	os.Setenv("EDP_ENABLE_DEBUG_LOGGING", "true")
+	logger.Setup()
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			// 프로세서 생성
+			np, err := processors.CreateProcessor(tC.processorName, nil)
+			if err != nil {
+				t.Error(err)
+			}
+			// 스테이지 러너 생성
+			fifo := pipelines.FIFO(np)
+
+			// 파라미터 생성
+			ctx, cancelFunc := context.WithCancel(context.Background())
+
+			// Stage 개수는 1개로 고정
+			// 채널 개수는 Stage 개수 +1
+			stageCh := make([]chan payloads.Payload, 1+1)
+			// 에러채널 개수는 Stage 개수 +2
+			errCh := make(chan error, 1+2)
+			for i := 0; i < len(stageCh); i++ {
+				stageCh[i] = make(chan payloads.Payload)
+			}
+
+			// FiFO Stage Runner 에게 넘길 파라미터 생성
+			wp := &workerParams{
+				stage: 0,
+				inCh:  stageCh[0],
+				outCh: stageCh[1],
+				errCh: errCh,
+			}
+			// StageRunner 구현체 FIFO 실행
+			// Goroutine 으로 실행
+			go fifo.Run(ctx, wp)
+
+			stageCh[0] <- tC.payload
+
+			for {
+				select {
+				case err := <-errCh:
+					if tC.testcase == "invalid" {
+						t.Log(err.Error())
+					} else {
+						t.Errorf(err.Error())
+					}
+					cancelFunc()
+					return
+				case data := <-stageCh[1]:
+					t.Log(data)
+					cancelFunc()
+					return
+				}
+			}
+		})
+	}
+}

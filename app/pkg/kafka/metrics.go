55ffbab6c6356f908061d41975538ae99b71f004
YeongSeok Kim <youngstone89@icloud.com>
2022-06-30 18:58:52 +0900

    added consumer read total metrics

---
+++ app/pkg/kafka/metrics.go
@@ -0,0 +1,22 @@
+package kafka
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	EDP_KAFKA_CONSUMER_READ_TOTAL      = "edp_kafka_consumer_read_total"
+	EDP_KAFKA_CONSUMER_READ_TOTAL_HELP = "the number of messages that kafka consumer reads in total"
+)
+
+var (
+	ConsumerReadTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: EDP_KAFKA_CONSUMER_READ_TOTAL,
+		Help: EDP_KAFKA_CONSUMER_READ_TOTAL_HELP,
+	}, []string{})
+)
+
+func init() {
+	prometheus.Register(ConsumerReadTotal)
+}
